c87675a66d8f1856145fa57c9abd45556cf03846
JinhwanB <paewhal20@gmail.com>
2023-03-24 16:41:30 +0900

    plus

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ const lineText = document.querySelector("#line_text");
 const strokeBtn = document.querySelector("#stroke");
 const squareBtn = document.querySelector("#square");
 const circleBtn = document.querySelector("#circle");
-const editBtn = document.querySelector("#edit");
+const textWidth = document.querySelector("#text_width");
+const editText = document.querySelector("#edit_text");
+const select = document.querySelector("select");
+const strokeTextBtn = document.querySelector("#stroke_text");
+const fillTextBtn = document.querySelector("#fill_text");
+const previewImg = document.querySelector("#preview");
 
 const ctx = canvas.getContext("2d");
 
 const CANVAS_SIZE = 800;
-const INVISIBLE_KEY = "invisible";
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
@@ -103,8 +107,12 @@ function circleClick() {
   mode = 5;
 }
 
-function editMode() {
-  text.classList.remove(INVISIBLE_KEY);
+function strokeTextBtnClick() {
+  mode = 6;
+}
+
+function fillTextBtnClick() {
+  mode = 7;
 }
 
 function colorClick(e) {
@@ -128,25 +136,40 @@ function onColorChange(e) {
 
 function doubleClick(e) {
   const textValue = text.value;
-  if (textValue !== "") {
+  const textSize = textWidth.value;
+  if (textValue !== "" && mode === 6) {
     ctx.save();
     ctx.lineWidth = 1;
-    ctx.font = "50px 맑은 고딕";
+    ctx.font = `${textSize}px 맑은 고딕`;
+    ctx.strokeText(textValue, e.offsetX, e.offsetY);
+  } else if (textValue !== "" && mode === 7) {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = `${textSize}px 맑은 고딕`;
     ctx.fillText(textValue, e.offsetX, e.offsetY);
-    text.value = "";
-    text.classList.add(INVISIBLE_KEY);
   }
 }
 
-function uploadFile(e) {
+function textWidthChange(e) {
+  const sizeOfText = e.target.value;
+  select.value = sizeOfText;
+}
+
+function selected(e) {
+  const sizeOfText = e.target.value;
+  textWidth.value = sizeOfText;
+}
+
+function previewFile(e) {
   const file = e.target.files[0];
   const url = URL.createObjectURL(file);
-  const image = new Image();
-  image.src = url;
-  image.onload = function () {
-    ctx.drawImage(image, 0, 0, CANVAS_SIZE, CANVAS_SIZE);
-  };
+  previewImg.src = url;
 }
+// const image = new Image();
+// image.src = url;
+// image.onload = function () {
+//   ctx.drawImage(image, 0, 0, CANVAS_SIZE, CANVAS_SIZE);
+// };
 
 function onSave() {
   const url = canvas.toDataURL();
@@ -178,10 +201,28 @@ strokeBtn.addEventListener("click", strokeClick);
 colorOptions.forEach((color) => color.addEventListener("click", colorClick));
 squareBtn.addEventListener("click", squareClick);
 circleBtn.addEventListener("click", circleClick);
-editBtn.addEventListener("click", editMode);
+strokeTextBtn.addEventListener("click", strokeTextBtnClick);
+fillTextBtn.addEventListener("click", fillTextBtnClick);
 
 widthRange.addEventListener("change", onWidthChange);
 colorBtn.addEventListener("change", onColorChange);
-fileInput.addEventListener("change", uploadFile);
+fileInput.addEventListener("change", previewFile);
+textWidth.addEventListener("change", textWidthChange);
+select.addEventListener("change", selected);
 
 document.addEventListener("keydown", cancelText);
+
+previewImg.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+previewImg.addEventListener("drop", (e) => {
+  e.preventDefault();
+  console.log(e.target.file[0]);
+});
+
+text.addEventListener("focusin", () => {
+  text.setAttribute("placeholder", "Write and doubleclick in canvas");
+});
+text.addEventListener("focusout", () => {
+  text.setAttribute("placeholder", "Write Here..");
+});
